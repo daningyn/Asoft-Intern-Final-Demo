1a51fda77df4a30577c6b13818ee3d87ad45adce
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-21 23:23:55 +0700

    + Create Project

---
+++ Asoft-Intern-Final-DemoTests/Asoft_Intern_Final_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Asoft_Intern_Final_DemoTests.swift
+//  Asoft-Intern-Final-DemoTests
+//
+//  Created by Danh Nguyen on 12/21/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import Asoft_Intern_Final_Demo
+
+class Asoft_Intern_Final_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
