a8ea29d23f600ff878999200b08bc7f1cd845161
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-22 00:28:13 +0700

    - Update Loading + Add Image, Icon

---
+++ Asoft-Intern-Final-Demo/View/LoadingVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingVC.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/22/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class LoadingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
