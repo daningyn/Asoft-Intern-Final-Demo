07ee9a3cfefdf7a1b4b2e5f8855feef022265c1f
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-23 00:29:39 +0700

    + Add Image for sign in and sign up collection view

---
+++ Asoft-Intern-Final-Demo/View/CustomViews/MarketingCollectionCell/MarketingCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MarketingCollectionViewCell.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/22/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MarketingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Asoft-Intern-Final-Demo/View/CustomViews/SignInCollectionCell/SignInCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SignInCollectionViewCell.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/22/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SignInCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
