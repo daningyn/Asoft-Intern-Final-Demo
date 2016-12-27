591aa9ec83e92d9c7ce588dcaa34d4d60886a517
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-27 16:53:03 +0700

    + Create ProfileView

---
+++ Asoft-Intern-Final-Demo/Defines/AppNavigationTitle.swift
@@ -0,0 +1,9 @@
+//
+//  AppNavigationTitle.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/27/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import Foundation
