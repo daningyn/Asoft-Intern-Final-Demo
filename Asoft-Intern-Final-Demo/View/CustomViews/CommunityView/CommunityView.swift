0a614dbd0add53404f9f9d31f2062c3f62299dfb
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-28 15:14:38 +0700

    + Create CommunityView

---
+++ Asoft-Intern-Final-Demo/View/CustomViews/CommunityView/CommunityView.swift
@@ -0,0 +1,15 @@
+//
+//  CommunityView.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/28/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CommunityView: UIView {
+
+    
+
+}
