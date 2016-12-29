4d92d512966886e7ea82eb0b9d86f2f402710e29
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-29 08:53:04 +0700

    + Add Icon, Image To Assets + Create ChefsViewController

---
+++ Asoft-Intern-Final-Demo/View/ChefsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChefsViewController.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/29/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ChefsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
