c9a09fe6c3dc4117dd0f1daf77e746e01a10552f
Danh Nguyen <danh.nghuu@gmail.com>
2016-12-30 08:56:44 +0700

    + Add Icon Heart + Create ChefQuoteCollectionViewCell

---
+++ Asoft-Intern-Final-Demo/View/ChefQuoteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChefQuoteCollectionViewCell.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 12/30/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ChefQuoteCollectionViewCell: UICollectionViewCell {
+    
+}
