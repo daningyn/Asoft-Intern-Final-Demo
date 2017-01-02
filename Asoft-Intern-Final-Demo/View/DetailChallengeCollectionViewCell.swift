6e7dfdd78561fe259f185f5a180adfad75c162b3
Danh Nguyen <danh.nghuu@gmail.com>
2017-01-02 14:59:35 +0700

    + Create TableView Cell

---
+++ Asoft-Intern-Final-Demo/View/DetailChallengeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DetailChallengeCollectionViewCell.swift
+//  Asoft-Intern-Final-Demo
+//
+//  Created by Danh Nguyen on 1/2/17.
+//  Copyright © 2017 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DetailChallengeCollectionViewCell: UICollectionViewCell {
+    
+}
